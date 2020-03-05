0c803887e26c37c186c0cceeede0dd6c61f41273
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-05 12:17:58 +0100

    adding stub to implement the mapping of R/C to subsidiaries

--- src/main/java/de/skymatic/model/AppleUtility.java
+++ src/main/java/de/skymatic/model/AppleUtility.java
@@ -3,7 +3,10 @@ package de.skymatic.model;
 public class AppleUtility {
 
 	public static Subsidiary mapRegionPlusCurrencyToSubsidiary(RegionPlusCurrency rpc){
-		//TODO
-		return Subsidiary.AMERICA;
+		switch (rpc){
+			//TODO:
+			default:
+				return Subsidiary.ROW;
+		}
 	}
 }
