f6b08eaa28263af2b7243221cfd3eb8f298c349a
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-05 12:17:27 +0100

    fixing test

--- src/test/java/de/skymatic/model/InvoiceTest.java
+++ src/test/java/de/skymatic/model/InvoiceTest.java
@@ -2,12 +2,14 @@ package de.skymatic.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class InvoiceTest {
 
 	@Test
 	public void testEmptyInvoiceReturnsZero() {
-		Invoice i = new Invoice();
+		Sales s = Mockito.mock(Sales.class);
+		Invoice i = new Invoice(RegionPlusCurrency.AMERICAS_USD, s);
 		Assertions.assertEquals(0, i.sum());
 	}
 }
