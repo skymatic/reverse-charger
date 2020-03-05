b3391ece99ea316d7bf878e98d43e5c89fb92d57
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-05 12:12:54 +0100

    adding constructor to monthlyInvoices class

--- src/main/java/de/skymatic/model/MonthlyInvoices.java
+++ src/main/java/de/skymatic/model/MonthlyInvoices.java
@@ -1,10 +1,30 @@
 package de.skymatic.model;
 
 import java.time.Month;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class MonthlyInvoices {
 
 	private Month month;
 	private Map<Subsidiary, Invoice> invoices;
+
+	public MonthlyInvoices (Month month, Invoice ... invoices){
+		this.month = month;
+		this.invoices = new Hashtable<>();
+		for(var i: invoices){
+			this.invoices.put(i.getSubsidiary(),i);
+		}
+	}
+
+	public void addEntry(Invoice i){
+		if(invoices.containsKey(i.getSubsidiary())){
+			throw new IllegalArgumentException("Subsidiary already exists!");
+		} else{
+			invoices.put(i.getSubsidiary(),i);
+		}
+	}
+
 }
