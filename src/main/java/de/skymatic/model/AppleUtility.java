0a46a2f55213bcd00071f4ea04c6721e5d78a514
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-05 12:11:44 +0100

    adding stub to map region/currency pairs to subsidaiaries

---
+++ src/main/java/de/skymatic/model/AppleUtility.java
@@ -0,0 +1,9 @@
+package de.skymatic.model;
+
+public class AppleUtility {
+
+	public static Subsidiary mapRegionPlusCurrencyToSubsidiary(RegionPlusCurrency rpc){
+		//TODO
+		return Subsidiary.AMERICA;
+	}
+}
