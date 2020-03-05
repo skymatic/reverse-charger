bd69d6c59b0b5a1df5627ff9c40e5f0b0ae97652
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-05 12:12:37 +0100

    adding constructor to invoice class

--- src/main/java/de/skymatic/model/Invoice.java
+++ src/main/java/de/skymatic/model/Invoice.java
@@ -1,10 +1,22 @@
 package de.skymatic.model;
 
+import java.util.Hashtable;
 import java.util.Map;
 
 public class Invoice {
 
+	private Subsidiary subsidiary;
 	private Map<RegionPlusCurrency,Sales> salesPerCountryPlusCurrency;
+
+	public Invoice(RegionPlusCurrency rpc, Sales s){
+		this.subsidiary = AppleUtility.mapRegionPlusCurrencyToSubsidiary(rpc);
+		this.salesPerCountryPlusCurrency = new Hashtable<>();
+		salesPerCountryPlusCurrency.put(rpc, s);
+	}
+
+	public Subsidiary getSubsidiary(){
+		return subsidiary;
+	}
 
 	public double sum() {
 		return salesPerCountryPlusCurrency.values().stream().mapToDouble(Sales::getProceeds).sum();
