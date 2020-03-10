a6a3adb8bc0580ca894061a98f19f38cc28013f3
Martin Beyer <37272740+BeyerMart@users.noreply.github.com>
2020-03-10 12:00:17 +0100

    Added possibility to parse static file from UI

--- src/main/java/de/skymatic/app/PrimaryController.java
+++ src/main/java/de/skymatic/app/PrimaryController.java
@@ -1,12 +1,29 @@
 package de.skymatic.app;
 
-import java.io.IOException;
+import de.skymatic.parser.CSVParser;
+import de.skymatic.parser.InvoiceParser;
 import javafx.fxml.FXML;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+	@FXML
+	private void switchToSecondary() throws IOException {
+		App.setRoot("secondary");
+	}
+
+	@FXML
+	private void startParsing() {
+		System.out.println("Working Directory = " +
+				System.getProperty("user.dir"));
+		Path path = Path.of("D:\\skymatic\\appstore-reverse-parser\\financial_report.csv");
+		CSVParser csvParser = new InvoiceParser();
+		try {
+			csvParser.parseCSV(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
