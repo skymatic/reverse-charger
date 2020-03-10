fb2852656750b9fa4f03e41161d28a1002895d60
Martin Beyer <37272740+BeyerMart@users.noreply.github.com>
2020-03-10 11:49:09 +0100

    Created method to add Sales for a single invoice

--- src/main/java/de/skymatic/model/Invoice.java
+++ src/main/java/de/skymatic/model/Invoice.java
@@ -6,19 +6,29 @@ import java.util.Map;
 public class Invoice {
 
 	private Subsidiary subsidiary;
-	private Map<RegionPlusCurrency,Sales> salesPerCountryPlusCurrency;
+	private Map<RegionPlusCurrency, Sales> salesPerCountryPlusCurrency;
 
-	public Invoice(RegionPlusCurrency rpc, Sales s){
+	public Invoice(RegionPlusCurrency rpc, Sales s) {
 		this.subsidiary = AppleUtility.mapRegionPlusCurrencyToSubsidiary(rpc);
 		this.salesPerCountryPlusCurrency = new Hashtable<>();
 		salesPerCountryPlusCurrency.put(rpc, s);
 	}
 
-	public Subsidiary getSubsidiary(){
+	public Subsidiary getSubsidiary() {
 		return subsidiary;
 	}
 
 	public double sum() {
 		return salesPerCountryPlusCurrency.values().stream().mapToDouble(Sales::getProceeds).sum();
+	}
+
+	public void addSales(RegionPlusCurrency rpc, Sales s) {
+		if (salesPerCountryPlusCurrency.containsKey(rpc)) {
+			throw new IllegalArgumentException("RegionPlusCurrency already exists!");
+		} else if (subsidiary != AppleUtility.mapRegionPlusCurrencyToSubsidiary(rpc)) {
+			throw new IllegalArgumentException("RegionPlusCurrency does not belong to this Subsidiary");
+		} else {
+			salesPerCountryPlusCurrency.put(rpc, s);
+		}
 	}
 }
