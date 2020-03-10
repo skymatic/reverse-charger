f7d741ff2cfe6df7d3985056af3e95983c79f778
Martin Beyer <37272740+BeyerMart@users.noreply.github.com>
2020-03-10 11:55:27 +0100

    Extended Interface CSVParser to throw Exception

--- src/main/java/de/skymatic/parser/CSVParser.java
+++ src/main/java/de/skymatic/parser/CSVParser.java
@@ -2,10 +2,11 @@ package de.skymatic.parser;
 
 import de.skymatic.model.MonthlyInvoices;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface CSVParser {
 
-	MonthlyInvoices parseCSV(Path p);
+	MonthlyInvoices parseCSV(Path p) throws IOException;
 
 }
