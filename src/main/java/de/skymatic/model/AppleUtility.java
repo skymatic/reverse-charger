0e64ea485fc813699c8d8e55383abe0b8bf11cb2
Martin Beyer <37272740+BeyerMart@users.noreply.github.com>
2020-03-16 22:44:42 +0100

    Added mapping from Regions to Subsidiaries according to "Apple Developer Program License Agreement"

--- src/main/java/de/skymatic/model/AppleUtility.java
+++ src/main/java/de/skymatic/model/AppleUtility.java
@@ -5,6 +5,16 @@ public class AppleUtility {
 	public static Subsidiary mapRegionPlusCurrencyToSubsidiary(RegionPlusCurrency rpc){
 		switch (rpc){
 			//TODO:
+			case CANADA_CAD:
+				return Subsidiary.CANADA;
+			case AUSTRALIA_AUD:
+			case NEW_ZEALAND_NZD:
+				return Subsidiary.AUSTRALIA;
+			case BRAZIL_BRL:
+			case COLOMBIA_COP:
+			case MEXICO_MXN:
+			case AMERICAS_USD:
+				return Subsidiary.AMERICA;
 			default:
 				return Subsidiary.ROW;
 		}
