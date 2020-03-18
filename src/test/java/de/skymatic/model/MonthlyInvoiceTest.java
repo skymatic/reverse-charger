ea328e8545cabcf3bce3ff64f0e8abdda513b72e
Martin Beyer <37272740+BeyerMart@users.noreply.github.com>
2020-03-18 12:27:02 +0100

    Adding test for MonthlyInvoices class

---
+++ src/test/java/de/skymatic/model/MonthlyInvoiceTest.java
@@ -0,0 +1,19 @@
+package de.skymatic.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.YearMonth;
+
+public class MonthlyInvoiceTest {
+
+	@Test
+	public void testAddingSalesEntryOfNoneExistingInvoiceCreatesIt() {
+		MonthlyInvoices monthlyInvoices = new MonthlyInvoices(YearMonth.of(2020, 3));
+		SalesEntry salesEntry = Mockito.mock(SalesEntry.class);
+		Mockito.when(salesEntry.getRpc()).thenReturn(RegionPlusCurrency.AMERICAS_USD);
+		monthlyInvoices.addSalesEntry(salesEntry);
+		Assertions.assertEquals(1, monthlyInvoices.getInvoices().size());
+	}
+}
