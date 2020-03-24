e1245aa7d9cd9268894351dc6ba9616162c21e38
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-24 16:23:05 +0100

    Enhance error information during parse

--- src/main/java/de/skymatic/parser/ParseException.java
+++ src/main/java/de/skymatic/parser/ParseException.java
@@ -3,13 +3,19 @@ package de.skymatic.parser;
 public class ParseException extends Exception {
 
 	private final Throwable cause;
+	private final String message;
 
-	public ParseException(Throwable cause) {
+	public ParseException(String message, Throwable cause) {
+		this.message = message;
 		this.cause = cause;
 	}
 
-	public Throwable getCause(){
+	public Throwable getCause() {
 		return cause;
+	}
+
+	public String getMessage() {
+		return message;
 	}
 
 }
