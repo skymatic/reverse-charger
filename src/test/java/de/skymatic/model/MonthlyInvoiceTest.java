b29b6d96b1406a87e242b6bf8240fa0ac1f29aff
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-27 17:26:57 +0100

    add tests for monthlyInvoices

--- src/test/java/de/skymatic/model/MonthlyInvoiceTest.java
+++ src/test/java/de/skymatic/model/MonthlyInvoiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.LocalDate;
 import java.time.YearMonth;
 
 public class MonthlyInvoiceTest {
@@ -11,9 +12,52 @@ public class MonthlyInvoiceTest {
 	@Test
 	public void testAddingSalesEntryOfNoneExistingInvoiceCreatesIt() {
 		MonthlyInvoices monthlyInvoices = new MonthlyInvoices(YearMonth.of(2020, 3), "CVS", 0);
+		Assertions.assertEquals(0, monthlyInvoices.getInvoices().size());
+
 		SalesEntry salesEntry = Mockito.mock(SalesEntry.class);
 		Mockito.when(salesEntry.getRpc()).thenReturn(RegionPlusCurrency.AMERICAS_USD);
 		monthlyInvoices.addSalesEntry(salesEntry);
 		Assertions.assertEquals(1, monthlyInvoices.getInvoices().size());
 	}
+
+	@Test
+	public void testAddingSalesEntryOfExistingSubsidiaryAddsIt() {
+		SalesEntry salesEntry = Mockito.mock(SalesEntry.class);
+		Mockito.when(salesEntry.getRpc()).thenReturn(RegionPlusCurrency.AMERICAS_USD);
+		MonthlyInvoices monthlyInvoices = new MonthlyInvoices(YearMonth.of(2020, 3), "CVS", 0, salesEntry);
+		int expected = monthlyInvoices.getInvoices().size();
+
+		SalesEntry other = Mockito.mock(SalesEntry.class);
+		Mockito.when(other.getRpc()).thenReturn(RegionPlusCurrency.MEXICO_MXN);
+		monthlyInvoices.addSalesEntry(other);
+		Assertions.assertEquals(expected, monthlyInvoices.getInvoices().size());
+	}
+
+	@Test
+	public void testChangingNumberOfNotExistingInvoiceThrowsException() {
+		MonthlyInvoices monthlyInvoices = new MonthlyInvoices(YearMonth.of(2020, 3), "CVS", 0);
+		Assertions.assertThrows(IllegalArgumentException.class, () -> monthlyInvoices.changeInvoiceNumber(Subsidiary.JAPAN, "CVS-333"));
+	}
+
+	@Test
+	public void testChangingIssueDateOfNotExistingInvoiceThrowsException() {
+		MonthlyInvoices monthlyInvoices = new MonthlyInvoices(YearMonth.of(2020, 3), "CVS", 0);
+		Assertions.assertThrows(IllegalArgumentException.class, () -> monthlyInvoices.changeIssueDate(Subsidiary.JAPAN, LocalDate.now()));
+	}
+
+	@Test
+	public void testDefaultNumberingIncreasesWithNewInvoice() {
+		int numberingSeed = 5;
+		SalesEntry salesEntry = Mockito.mock(SalesEntry.class);
+		Mockito.when(salesEntry.getRpc()).thenReturn(RegionPlusCurrency.AMERICAS_USD);
+		MonthlyInvoices monthlyInvoices = new MonthlyInvoices(YearMonth.of(2020, 3), "CVS", numberingSeed, salesEntry);
+
+		SalesEntry other = Mockito.mock(SalesEntry.class);
+		Mockito.when(other.getRpc()).thenReturn(RegionPlusCurrency.JAPAN_JPY);
+		monthlyInvoices.addSalesEntry(other);
+
+		Assertions.assertTrue(monthlyInvoices.getInvoices().stream().anyMatch(invoice -> invoice.getNumberString().endsWith(String.valueOf(numberingSeed))));
+		Assertions.assertTrue(monthlyInvoices.getInvoices().stream().anyMatch(invoice -> invoice.getNumberString().endsWith(String.valueOf(numberingSeed + 1))));
+	}
+
 }
