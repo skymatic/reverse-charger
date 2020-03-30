f7b865a5236265559af3ae5381be74ce5f1fce47
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-30 14:59:43 +0200

    addint test stubs for PlaceholderReplacer and AppleParser

---
+++ src/test/java/de/skymatic/output/PlaceHolderReplacerTest.java
@@ -0,0 +1,17 @@
+package de.skymatic.output;
+
+import org.junit.jupiter.api.Test;
+
+public class PlaceHolderReplacerTest {
+
+	@Test
+	public void testOnlyKnownPlaceholdersAreReplaced(){
+		//TODO
+	}
+
+	@Test
+	public void testForEachInvoiceExistsEntry(){
+		//TODO
+	}
+
+}
