55ac7e2b92a726daffbf628b330598738ba1ded1
Armin Schrenk <armin.schrenk@zoho.eu>
2020-03-31 14:26:31 +0200

    create test class with stubs for settings

---
+++ src/test/java/de/skymatic/settings/SettingsJsonAdapterTest.java
@@ -0,0 +1,17 @@
+package de.skymatic.settings;
+
+import org.junit.jupiter.api.Test;
+
+public class SettingsJsonAdapterTest {
+
+	@Test
+	public void testSerialize(){
+		//TODO: from settings to JSON
+	}
+
+	@Test
+	public void testDeserialize(){
+		//TODO: from JSON to Settings
+	}
+
+}
