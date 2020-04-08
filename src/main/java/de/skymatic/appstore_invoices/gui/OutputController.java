5ccd0488b038a76a95bf202e1083d75f7008ac51
Martin Beyer <beyer.martin1998@gmail.com>
2020-04-08 12:16:17 +0200

    Moved UI-elements from Primary to OutputController

--- src/main/java/de/skymatic/appstore_invoices/gui/OutputController.java
+++ src/main/java/de/skymatic/appstore_invoices/gui/OutputController.java
@@ -1,12 +1,214 @@
 package de.skymatic.appstore_invoices.gui;
 
+import de.skymatic.appstore_invoices.model.Invoice;
+import de.skymatic.appstore_invoices.model.MonthlyInvoices;
+import de.skymatic.appstore_invoices.output.HTMLGenerator;
+import de.skymatic.appstore_invoices.output.HTMLWriter;
+import de.skymatic.appstore_invoices.settings.Settings;
+import de.skymatic.appstore_invoices.settings.SettingsProvider;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.ObjectBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Map;
+import java.util.Optional;
 
 public class OutputController {
 
+	@FXML
+	private TableColumn<Invoice, String> columnInvoiceNumber;
+	@FXML
+	private TableColumn<Invoice, String> columnSubsidiary;
+	@FXML
+	private TableColumn<Invoice, String> columnAmount;
+	@FXML
+	private TableColumn<Invoice, String> columnProceeds;
+	@FXML
+	private CheckBox persistSettingsCheckBox;
+	@FXML
+	private RadioButton externalTemplateRadioButton;
+	@FXML
+	private RadioButton storedTemplateRadioButton;
+	private final ObjectBinding<Path> templatePath;
+	private final ObjectBinding<Path> outputPath;
+	private final HTMLGenerator htmlGenerator;
+	private final Path defaultTemplatePath;
+	private final ObservableList<Invoice> invoices;
 	private final Stage stage;
+	private final SettingsProvider settingsProvider;
+	private final BooleanProperty isReadyToGenerate;
 
-	public OutputController(Stage stage) {
+	private Settings settings;
+	private MonthlyInvoices monthlyInvoices;
+
+
+	public OutputController(Stage stage, SettingsProvider settingsProvider, MonthlyInvoices monthlyInvoices) {
 		this.stage = stage;
+		this.settingsProvider = settingsProvider;
+		settings = settingsProvider.get();
+		this.invoices = FXCollections.observableArrayList();
+		isReadyToGenerate = new SimpleBooleanProperty(false);
+		invoices.addListener((ListChangeListener) (e -> updateIsReadyToGenerate()));
+		defaultTemplatePath = settingsProvider.getStoragePath().resolve(Settings.STORED_TEMPLATE_NAME);
+		templatePath = Bindings.createObjectBinding(() -> {
+			if (settings.isUsingExternalTemplate()) {
+				return Path.of(settings.getExternalTemplatePath());
+			} else {
+				return defaultTemplatePath;
+			}
+		}, settings.externalTemplatePathProperty(), settings.usingExternalTemplateProperty());
+		outputPath = Bindings.createObjectBinding(() -> Path.of(settings.getOutputPath()), settings.outputPathProperty());
+		outputPath.addListener(o -> updateIsReadyToGenerate());
+
+		this.monthlyInvoices = monthlyInvoices;
+		htmlGenerator = new HTMLGenerator();
 	}
+
+	@FXML
+	public void initialize() {
+		columnInvoiceNumber.setCellFactory(TextFieldTableCell.<Invoice>forTableColumn());
+		columnInvoiceNumber.setCellValueFactory(invoice -> new SimpleStringProperty(invoice.getValue().getNumberString()));
+		columnInvoiceNumber.setOnEditCommit((TableColumn.CellEditEvent<Invoice, String> event) -> {
+			String newNumberString = event.getNewValue();
+			Invoice invoice = event.getRowValue();
+			if (invoices.stream()
+					.filter(i -> !i.equals(invoice))
+					.anyMatch(i -> i.getNumberString().equals(newNumberString))) {
+				Alerts.duplicateInvoiceNumber()
+						.showAndWait();
+				columnInvoiceNumber.getTableView().refresh();
+				columnInvoiceNumber.getTableView().requestFocus();
+			} else {
+				monthlyInvoices.changeSingleInvoiceNumber(invoice.getSubsidiary(), newNumberString);
+			}
+		});
+
+		columnSubsidiary.setCellValueFactory(invoice -> new ReadOnlyObjectWrapper<>(invoice.getValue().getSubsidiary().toString()));
+		columnAmount.setCellFactory(column -> {
+			var cell = new TextFieldTableCell<Invoice, String>();
+			cell.setAlignment(Pos.BASELINE_RIGHT);
+			return cell;
+		});
+		columnAmount.setCellValueFactory(invoice -> new ReadOnlyObjectWrapper<>(String.valueOf(invoice.getValue().getAmount())));
+
+		columnProceeds.setCellFactory(column -> {
+			var cell = new TextFieldTableCell<Invoice, String>();
+			cell.setAlignment(Pos.BASELINE_RIGHT);
+			return cell;
+		});
+		columnProceeds.setCellValueFactory(invoice -> new ReadOnlyObjectWrapper<>((String.format("%.2f", invoice.getValue().sum()))));
+
+		persistSettingsCheckBox.setSelected(settings.isSaveAndOverwriteSettings());
+		settings.saveAndOverwriteSettingsProperty().bind(persistSettingsCheckBox.selectedProperty());
+
+		if (settings.isUsingExternalTemplate()) {
+			externalTemplateRadioButton.setSelected(true);
+		} else {
+			storedTemplateRadioButton.setSelected(true);
+		}
+		settings.usingExternalTemplateProperty().bind(externalTemplateRadioButton.selectedProperty());
+
+		//TODO: set the checkbox according to the settings
+	}
+
+	private void updateIsReadyToGenerate() {
+		isReadyToGenerate.setValue(!(settings.getOutputPath().isEmpty() || invoices.isEmpty()));
+	}
+
+	@FXML
+	private void replaceStoredTemplate() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Template file");
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("HTML template file", "*.html", "*.htm"));
+		File selectedFile = fileChooser.showOpenDialog(stage);
+		if (selectedFile != null) {
+			try {
+				Path tmpFile = settingsProvider.getStoragePath().resolve("tmp.html");
+				Files.copy(selectedFile.toPath(), tmpFile);
+				Files.move(tmpFile, defaultTemplatePath, StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				//TODO: better error Handling
+				Alerts.genericError(e, "Replacing the stored template by the new one.").showAndWait();
+			}
+		}
+	}
+
+	@FXML
+	private void chooseExternalTemplate() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Template file");
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("HTML template file", "*.html", "*.htm"));
+		File selectedFile = fileChooser.showOpenDialog(stage);
+		if (selectedFile != null) {
+			settings.setExternalTemplatePath(selectedFile.toPath().toString());
+		} else {
+			storedTemplateRadioButton.setSelected(true);
+		}
+	}
+
+	@FXML
+	private void chooseOutputDirectory() {
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		directoryChooser.setTitle(("Choose directory to save output to"));
+		File selectedDirectory = directoryChooser.showDialog(stage);
+		if (selectedDirectory != null) {
+			settings.setOutputPath(selectedDirectory.toPath().toString());
+		}
+	}
+
+	@FXML
+	public void generateInvoices() {
+		if (settings.isSaveAndOverwriteSettings()) {
+			try {
+				settingsProvider.save(settings);
+			} catch (IOException e) {
+				//TODO: better error handling
+				Alerts.genericError(e, "Saving settings on hard disk.").showAndWait();
+			}
+		}
+		try {
+			Map<String, StringBuilder> htmlInvoices = htmlGenerator.createHTMLInvoices(templatePath.get(), monthlyInvoices.getInvoices());
+			new HTMLWriter().write(outputPath.get(), htmlInvoices);
+		} catch (IOException e) {
+			//TODO: better error handling
+			Alerts.genericError(e, "Generating the invoices from template and save them to hard disk.").showAndWait();
+		}
+
+	}
+
+
+	// Getter & Setter
+
+	public BooleanProperty isReadyToGenerateProperty() {
+		return isReadyToGenerate;
+	}
+
+	public Boolean getIsReadyToGenerate() {
+		return isReadyToGenerate.get();
+	}
+
+	public ObservableList<Invoice> getInvoices() {
+		return invoices;
+	}
+
 }
