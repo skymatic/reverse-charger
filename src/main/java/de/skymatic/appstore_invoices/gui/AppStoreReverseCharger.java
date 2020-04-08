a9f5d2a7d230cf824c6a7f27c1318c8c8136a36d
Martin Beyer <beyer.martin1998@gmail.com>
2020-04-08 10:02:33 +0200

    Refactoring to bundle FxmlLoading and Controller creation in one class

--- src/main/java/de/skymatic/appstore_invoices/gui/AppStoreReverseCharger.java
+++ src/main/java/de/skymatic/appstore_invoices/gui/AppStoreReverseCharger.java
@@ -1,8 +1,6 @@
 package de.skymatic.appstore_invoices.gui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -14,28 +12,17 @@ import java.io.IOException;
 public class AppStoreReverseCharger extends Application {
 
 	private static final String WINDOW_TITLE = "Appstore reverse charger";
-	
-	private static Scene scene;
-	private static Stage stage;
+
 
 	@Override
 	public void start(Stage stage) throws IOException {
-		this.stage = stage;
-		scene = new Scene(loadFXML("primary", stage));
+		FxmlLoader fxmlLoader = new FxmlLoader(stage);
+		Scene scene = fxmlLoader.createScene("primary");
 		stage.setScene(scene);
 		stage.setTitle(WINDOW_TITLE);
 		stage.show();
 	}
 
-	static void setRoot(String fxml) throws IOException {
-		scene.setRoot(loadFXML(fxml, stage));
-	}
-
-	private static Parent loadFXML(String fxml, Stage owner, Object... parameters) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(AppStoreReverseCharger.class.getResource(fxml + ".fxml"));
-		fxmlLoader.setControllerFactory(new ControllerFactory(owner, parameters));
-		return fxmlLoader.load();
-	}
 
 	public static void main(String[] args) {
 		launch();
