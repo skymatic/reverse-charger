faa607d7bccdccf17a8029fd0344845253c91bb5
Armin Schrenk <armin.schrenk@zoho.eu>
2020-07-02 14:41:47 +0200

    Removing CSVParser interface

--- src/main/java/de/skymatic/appstore_invoices/parser/AppleParser.java
+++ src/main/java/de/skymatic/appstore_invoices/parser/AppleParser.java
@@ -1,6 +1,5 @@
 package de.skymatic.appstore_invoices.parser;
 
-import de.skymatic.appstore_invoices.model.InvoiceCollection;
 import de.skymatic.appstore_invoices.model.apple.AppleReport;
 import de.skymatic.appstore_invoices.model.apple.AppleSalesEntry;
 import de.skymatic.appstore_invoices.model.apple.RegionPlusCurrency;
@@ -14,12 +13,12 @@ import java.time.YearMonth;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-public class AppleParser implements CSVParser, ReportParser {
+public class AppleParser implements ReportParser {
 
-	private static final int MIN_COLUMN_COUNT = 10;
 	private boolean endOfReport = false;
 
-	public ParseResult parseCSV(Path p) throws IOException, OldParseException {
+	@Override
+	public AppleReport parse(Path p) throws IOException, ReportParseException, IllegalArgumentException {
 		StringReference lastReadLine = new StringReference();
 		try (BufferedReader br = Files.newBufferedReader(p)) {
 			String header = br.readLine();
@@ -43,9 +42,9 @@ public class AppleParser implements CSVParser, ReportParser {
 						return new AppleSalesEntry(rpc, units, earned, pretaxSubtotal, inputTax, adjustments, withholdingTax, totalOwned, exchangeRate, proceeds);
 					}).collect(Collectors.toList());
 
-			return new ParseResult(yearMonth, sales);
+			return new AppleReport(yearMonth, "", 1, sales.toArray(new AppleSalesEntry[]{}));
 		} catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
-			throw new OldParseException("Error parsing line:  " + lastReadLine.get(), e);
+			throw new ReportParseException("Error parsing line:  " + lastReadLine.get(), -1, e);
 		}
 	}
 
@@ -61,14 +60,4 @@ public class AppleParser implements CSVParser, ReportParser {
 				.toUpperCase());
 	}
 
-	@Override
-	public InvoiceCollection parse(Path p) throws IOException, ReportParseException, IllegalArgumentException {
-		try {
-			//TODO: this is currently only for compiling purpose. AppleReport needs to be refactored.
-			ParseResult result = parseCSV(p);
-			return new AppleReport(result.getYearMonth(), "", 1, result.getSales().toArray(new AppleSalesEntry[]{}));
-		} catch (OldParseException e) {
-			throw new ReportParseException(e.getMessage(), -1, e.getCause());
-		}
-	}
 }
