fd6a5b9503405b966190de95d0ee0eaea686ce2d
Armin Schrenk <armin.schrenk@zoho.eu>
2020-07-02 12:45:35 +0200

    Refacterd GoogleReportTest: * sale instances are now generated by googleSaleFactory * removed test of not exisisting country

--- src/test/java/de/skymatic/appstore_invoices/model/google/GoogleReportTest.java
+++ src/test/java/de/skymatic/appstore_invoices/model/google/GoogleReportTest.java
@@ -3,8 +3,9 @@ package de.skymatic.appstore_invoices.model.google;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
 import java.time.YearMonth;
+
+import static de.skymatic.appstore_invoices.model.google.GoogleSaleFactory.*;
 
 public class GoogleReportTest {
 
@@ -23,96 +24,19 @@ public class GoogleReportTest {
 
 	@Test
 	public void addingSaleOfNotExistingSubsidiaryCreatesIt() {
-		//TODO: Fails due to unknown country
 		GoogleReport report = new GoogleReport(YearMonth.now());
-		Assertions.assertTrue(report.toInvoices().isEmpty());
-		report.add(getValidSale());
-		Assertions.assertEquals(1, report.toInvoices().size());
+		int expectedSize = report.toInvoices().size() + 1;
+		report.add(getSale());
+		Assertions.assertEquals(expectedSize, report.toInvoices().size());
 	}
 
 	@Test
 	public void addingSaleOfExistingSubsidiaryDoesNotChangeNumberOfInvoices() {
-		//TODO: Fails due to unknown country
 		GoogleReport report = new GoogleReport(YearMonth.now());
-		Assertions.assertTrue(report.toInvoices().isEmpty());
-		report.add(getValidSale());
-		report.add(getValidSaleOfSameSubsidiary());
-		Assertions.assertEquals(1, report.toInvoices().size());
+		int expectedSize = report.toInvoices().size() + 1;
+		report.add(getSale());
+		report.add(getSaleOfDifferentProduct());
+		Assertions.assertEquals(expectedSize, report.toInvoices().size());
 	}
 
-	/**
-	 * TODO: refactor to its own CLass ( the functionality of GoogleUtility is tested!)
-	 */
-	@Test
-	public void addingSaleOfUnknownCountryThrowsException() {
-		GoogleReport report = new GoogleReport(YearMonth.now());
-		Assertions.assertThrows(IllegalArgumentException.class, () -> report.add(getInvalidCountrySale()));
-	}
-
-
-	private static GoogleSale getValidSale() {
-		return new GoogleSale("1234",
-				LocalDateTime.now(),
-				"tax",
-				GoogleTransactionType.CHARGE,
-				"refund",
-				"MyProduct",
-				"my.company",
-				0,
-				"",
-				"ultraPhone",
-				"DE",
-				"Bavaria",
-				"80333",
-				"EUR",
-				1337.0,
-				1.0,
-				"EUR",
-				1337.0
-		);
-	}
-
-	private static GoogleSale getValidSaleOfSameSubsidiary() {
-		return new GoogleSale("1236",
-				LocalDateTime.now(),
-				"tax",
-				GoogleTransactionType.CHARGE,
-				"refund",
-				"MyProduct",
-				"my.company",
-				0,
-				"",
-				"ultraPhone",
-				"DE",
-				"Bavaria",
-				"80333",
-				"EUR",
-				1337.0,
-				1.0,
-				"EUR",
-				1337.0
-		);
-	}
-
-	private static GoogleSale getInvalidCountrySale() {
-		return new GoogleSale("1234",
-				LocalDateTime.now(),
-				"tax",
-				GoogleTransactionType.CHARGE,
-				"refund",
-				"MyProduct",
-				"my.company",
-				0,
-				"",
-				"ultraPhone",
-				"XX",
-				"Bavaria",
-				"80333",
-				"EUR",
-				1337.0,
-				1.0,
-				"EUR",
-				1337.0
-		);
-	}
 }
